caf57975ae53da8a272da95637bbeef09d475abc
biacoronha <biacoronha@gmail.com>
2023-03-09 13:37:35 +0000

    update style

--- src/pages/login-page/loginPage.tsx
+++ src/pages/login-page/loginPage.tsx
@@ -1,12 +1,13 @@
 import { loginUrl } from '../../spotify'
 import './loginPage.css'
+import logo from '../../assets/Spotify_Logo_RGB_Green.png'
 
 export const LoginPage = () => {
     console.log('login page')
     return (
         <div className="login">
             <img
-                src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+                src={logo}  
                 alt="Spotify logo"
             />
             <a href={loginUrl}>LOGIN WITH SPOTIFY</a>
